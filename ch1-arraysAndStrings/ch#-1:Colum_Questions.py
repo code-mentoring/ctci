fed8b93666f0521eda5bfd9f4fe4e82ccc2b9b25
Colum Keane <columkeane@gmail.com>
2021-02-07 22:16:03 +0000

    Test Commit

---
+++ ch1-arraysAndStrings/ch#-1:Colum_Questions.py
@@ -0,0 +1,13 @@
+#Question 1.4
+def IsPalindrome ( S ) :
+    #Does the sequence 'S' read the same forewards and backwards?
+    S = "" 
+    start = 0
+    end = len ( S ) -1
+    while start < end :
+        if S [start] != S [end] :
+            return False
+        start +=1
+        end -= 1
+            
+    return True
