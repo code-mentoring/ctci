d0e0664c8fb569bfce935557bf4063f836139a3d
Rajat Bansal <rjtbansal@gmail.com>
2021-02-09 19:55:06 -0500

    Chapter 1: unique strings with additional data structure

---
+++ ch1-arraysAndStrings/rajatBansal__1_1.js
@@ -0,0 +1,38 @@
+/**1.1 - Is Unique
+Implement an algorithm to determine if a string has all unique characters. What if you
+cannot use additional data structures? */
+
+const isUnique = str => {
+
+  if (str.length <= 1) {
+    return true;
+  }
+
+  str = str.trim(); //remove trailing and leading whitespaces if any
+
+  const uniqueTracker = {};
+
+  for (let letter of str) {
+    if (uniqueTracker[letter]) {
+      uniqueTracker[letter] += 1;
+    }
+    else {
+      uniqueTracker[letter] = 1;
+    }
+  }
+
+  for (let letter in uniqueTracker) {
+    if (uniqueTracker[letter] > 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+//tests
+console.log(isUnique('helloworld')); //false
+console.log(isUnique('abcdef1234')); //true
+console.log(isUnique('  abcdef1234  ')); //true
+console.log(isUnique('  _abcdef1234__  ')); //false
