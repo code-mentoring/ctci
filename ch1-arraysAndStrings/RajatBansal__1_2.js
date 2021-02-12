2cd554f5684b481702cdf1062df93bb1cc2b9f80
Rajat Bansal <rjtbansal@gmail.com>
2021-02-11 21:21:25 -0500

    added check permutation solution

---
+++ ch1-arraysAndStrings/RajatBansal__1_2.js
@@ -0,0 +1,59 @@
+/**1.2 - Check Permutation
+Given two strings, write a method to decide if one is a permutation of the
+other. */
+
+const isPermutation = (str1, str2) => {
+  str1 = str1.trim();
+  str2 = str2.trim();
+
+  //corner cases
+  if (!str1.length && !str2.length) {
+    //both 0 or null
+    return true;
+  } else if (str1.length !== str2.length) {
+    return false;
+  } else if (str1.length === 1 && str2.length === 1) {
+    return str1 === str2;
+  }
+
+  const strTracker = {};
+  for (let character of str1) {
+    strTracker[character] = strTracker[character]
+      ? strTracker[character] + 1
+      : 1;
+  }
+
+  for (let character of str2) {
+    if (strTracker[character]) {
+      strTracker[character] -= 1;
+    } else {
+      return false;
+    }
+  }
+
+  for (let character in strTracker) {
+    if (strTracker[character] !== 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const testStrings = [
+  ["", ""], //true
+  ["a", "b"], //false
+  ["c", "c"], //true
+  ["  abcd ", " abce "], //false
+  ["abc123", "12abc3"], //true
+  ["godly love", "lovely god"],
+];
+console.log("Are following 2 strings combos?");
+
+for (let stringCombo of testStrings) {
+  const firstStr = stringCombo[0];
+  const secondStr = stringCombo[1];
+  console.log(
+    `${firstStr}, ${secondStr}: ${isPermutation(firstStr, secondStr)}`
+  );
+}
