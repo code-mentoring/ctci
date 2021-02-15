e037f40365ed1ec912d7e7084dde64e4e3fa982f
Colum Keane <columkeane@gmail.com>
2021-02-15 12:26:49 +0000

    Adding Solution to 1.4

--- ch1-arraysAndStrings/ch#-1:Colum_Questions.py
+++ ch1-arraysAndStrings/ch#-1:Colum_Questions.py
@@ -38,3 +38,53 @@ def IsPalindrome( S ):
         end -= 1
 
     return True
+
+"""
+Shane Sheridan's solution to question 1.4 Palindrome Permutation
+My Solution above does not cover all case scenarios.
+"""
+def is_palindrome_permutation(s):
+    # Palindromes ignore punctuation, spaces and case. E.g. 'No lemon, no melon'.
+    s = remove_punctuation(s)
+    
+    length = len(s)
+    if (length <= 1):
+        return False
+    
+    # If each character occurs even number of times, then it must be a palindrome.
+    # Except odd length palindromes which must have one extra character, i.e. the middle.
+    is_even_length = is_even(length)
+    found_odd_count = False
+    for c in s:
+        count = get_occurrences(c, s)
+        even_occurrence = is_even(count)
+        if (is_even_length and not(even_occurrence)):
+             return False
+        elif (not(is_even_length) and not(even_occurrence)):
+            # Allow for one extra character in odd length strings.
+            if (found_odd_count):
+                return False
+            else:
+                found_odd_count = True
+
+    return True
+
+def is_even(num):
+    return (num % 2 == 0)
+
+def get_occurrences(c, s):
+    count = 0
+    for i in s:
+        if i == c:
+            count += 1
+    
+    return count
+    
+def remove_punctuation(s):
+    # Removing '/' and including ' ' in python's built in 'string.punctuation'.
+    punctuation = r"""!"#$%&'()*+,-.:;<=>?@[\]^_`{|}~ """
+    for c in s:
+        if c in punctuation:
+            s = s.replace(c, "")
+
+    return s.lower()
