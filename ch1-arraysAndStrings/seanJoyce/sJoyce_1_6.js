ef4b10b28293a18c6a0600cf667ae35728b831a6
Sean Joyce <seanjoyce@Seans-MacBook-Air.local>
2021-02-24 05:40:31 +0000

    Worked on problem 1.6 from the CTCI using JavaScript

---
+++ ch1-arraysAndStrings/seanJoyce/sJoyce_1_6.js
@@ -0,0 +1,54 @@
+let string = 'aabcccccaaa';
+
+stringCompression('aabcccccaaa')
+
+
+//compress string --> then compare lengths 
+//don't repeatedly concatenate strings as this is inefficient 
+
+function stringCompression(string) {
+    //compress
+    let newString = "";
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== string[i - 1]) {
+            newString += string[i];
+        }
+        if ((i === string.length - 1) && (string[i] === string[i - 1])) {
+
+        } else if ((i === string.length - 1) && (string[i] !== string[i - 1])) {
+            newString += string[i];
+        }
+        else {
+            let myCount = 1;
+            while (string[i] === string[i + myCount]) {
+                myCount += 1;
+            }
+        }
+    }
+
+
+    //count
+    let stringArray = string.split("")
+    numbersArray = [];
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] !== string[i - 1]) {
+            let count = 1;
+            while (string[i] === string[i + count]) {
+                count++;
+            }
+            numbersArray.push(count)
+        }
+    }
+
+    //join
+    let numberString = numbersArray.toString()
+    numberString = numberString.replace(/,/g, "")
+    let jointString = [];
+    for (let i = 0; i < newString.length; i++) {
+        jointString += newString[i]
+        jointString += numberString[i]
+    }
+
+    console.log(jointString)
+    return jointString
+}
