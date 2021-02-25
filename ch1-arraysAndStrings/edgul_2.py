9271d8c635c6eb09f8e446a7d97998c3dfd68277
edgul <ed.guloien@gmail.com>
2021-02-25 00:03:58 -0500

    Improved zero matrix solution

--- ch1-arraysAndStrings/edgul_2.py
+++ ch1-arraysAndStrings/edgul_2.py
@@ -34,44 +34,47 @@ def compress2(inString : str) -> str:
             l[-1] = first+str(count)
     return "".join(l) # O(n) - faster than concatenation
 
+# helper function - transpose
+def transpose(matrix : "NxM list of lists of ints"):
+    return [[x[j] for i,x in enumerate(matrix)] for j,y in enumerate(matrix)] 
+
 # 1.7 - Rotate Matrix - rotate matrix by 90deg - O(n^2), which is okay for a matrix
 def rotateMatrix(matrix : list) -> list:
     n = len(matrix)
-
-    # transpose
-    transpose = [[0 for y in matrix[0]] for x in matrix]
-    for i in range(n):
-        for j in range(n):
-            transpose[i][j] = matrix[j][i]
+    trans = transpose(matrix)
 
     # shift
     for i in range(n):
         for j in range(int(n/2)):
-            temp = transpose[i][j]
-            transpose[i][j] = transpose[i][n-1-j]
-            transpose[i][n-1-j] = temp
-    return transpose
+            temp = trans[i][j]
+            trans[i][j] = trans[i][n-1-j]
+            trans[i][n-1-j] = temp
+    return trans
 
-# 1.8 - Zero Matrix - clear row and column of found 0 element - 0(n^4) for NxN matrix
+# 1.8 - Zero Matrix - clear all rows and columns of found 0 elements 
+#     - 0(NxM) for NxM matrix
 def zeroMatrix(matrix : 'list of lists') -> 'list of lists':
-    output = copy.deepcopy(matrix)
 
-    # list of tuples
-    pointsToZero = []
-    for i,row in enumerate(matrix):
+    # clear row if we find a zero in the row
+    rows = copy.deepcopy(matrix) 
+    for i,row in enumerate(rows):
+        if row.count(0) > 0:
+            rows[i] = [0 for x in range(len(rows))]
+
+    # clear col if we find a zero in the col
+    columns = transpose(matrix)  
+    for j,col in enumerate(columns):
+        if col.count(0) > 0:
+            columns[j] = [0 for x in range(len(columns))]
+
+    # merge the matrices
+    for i, row in enumerate(rows):
         for j,item in enumerate(row):
-            if item == 0:
-                pointsToZero.append((i,j))
-
-    # this part is slow 
-    for x,y in pointsToZero:
-        for i in range(len(output)):
-            for j in range(len(output[i])):
-                if x == i or y == j:
-                    output[i][j] = 0
-    return output
+            rows[i][j] = item and columns[j][i] # note index reversal for columns
+    return rows 
 
 def testZeroMatrix():
+    # 2x2
     matrix = [[1,2], [3,4]]
     assert zeroMatrix(matrix) == [[1,2], [3,4]]
     matrix = [[0,2], [3,4]]
@@ -79,6 +82,7 @@ def testZeroMatrix():
     matrix = [[1,0], [3,4]]
     assert zeroMatrix(matrix) == [[0,0], [3,0]]
 
+    # 3x3
     matrix = [[1,2,3], [4,5,6], [7,8,9]]
     assert zeroMatrix(matrix) == [[1,2,3], [4,5,6], [7,8,9]]
     matrix = [[0,2,3], [4,5,6], [7,8,9]]
