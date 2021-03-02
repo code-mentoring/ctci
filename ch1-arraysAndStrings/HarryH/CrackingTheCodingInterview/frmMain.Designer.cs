608696f7c4caf77b4f96a14c49edfa17107d4098
Harry0227 <79758939+Harry0227@users.noreply.github.com>
2021-03-02 14:02:31 -0500

    Create a solution for testing.
    Create a solution for testing.

---
+++ ch1-arraysAndStrings/HarryH/CrackingTheCodingInterview/frmMain.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace CrackingTheCodingInterview
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStringCompression = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnStringCompression
+            // 
+            this.btnStringCompression.Location = new System.Drawing.Point(402, 12);
+            this.btnStringCompression.Name = "btnStringCompression";
+            this.btnStringCompression.Size = new System.Drawing.Size(81, 38);
+            this.btnStringCompression.TabIndex = 0;
+            this.btnStringCompression.Text = "String Compression";
+            this.btnStringCompression.UseVisualStyleBackColor = true;
+            this.btnStringCompression.Click += new System.EventHandler(this.btnStringCompression_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(26, 22);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(178, 20);
+            this.txtInput.TabIndex = 1;
+            this.txtInput.Text = "aabcccccaaa";
+            // 
+            // txtOut
+            // 
+            this.txtOut.Location = new System.Drawing.Point(227, 22);
+            this.txtOut.Name = "txtOut";
+            this.txtOut.Size = new System.Drawing.Size(153, 20);
+            this.txtOut.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 73);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnStringCompression);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Cracking The Coding Interview";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStringCompression;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOut;
+    }
+}
+
---
+++ ch1-arraysAndStrings/HarryH/CrackingTheCodingInterview/frmMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CrackingTheCodingInterview
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStringCompression_Click(object sender, EventArgs e)
+        {
+            string inputString;
+            string outputString;
+
+            inputString = txtInput.Text;
+            outputString=CompressString(inputString);
+
+            txtOut.Text = outputString;
+
+        }
+
+        private static string CompressString(string str)
+        {
+            string compressedString = "";
+            int countConsecutive = 0;
+            for (int i=0; i< str.Length; i++)
+            {
+                countConsecutive++;
+
+                if (i+1>= str.Length || str.Substring(i,1)!=str.Substring(i+1,1))
+                {
+                    compressedString += "" + str.Substring(i, 1) + countConsecutive;
+                    countConsecutive = 0;
+
+                }
+            }
+
+            return compressedString;
+
+        }
+
+    }
+
+    
+    
+}
