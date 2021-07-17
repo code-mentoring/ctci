b4c0fe9ed7bfc760fc8bac496f639b2549bf47d8
edgul <ed.guloien@gmail.com>
2021-07-17 13:53:07 -0400

    Added 5.4 for ed

--- ch5-bit-manipulation/edgul/main.cpp
+++ ch5-bit-manipulation/edgul/main.cpp
@@ -1,22 +1,73 @@
 
 #include <iostream>
 #include <bitset>
+#include <climits>
 
 struct MinAndMax
 {
-    int min;
-    int max;
+    int min = 0;
+    int max = 0;
+    bool valid = false;
 };
 
-// 4.4
+int countBits(unsigned short x)
+{
+    int count = 0;
+    for (int i = 0; i < 16; i++)
+    {
+        if (x >> i & 0x1) count++;
+    }
+    return count;
+}
+
+// 5.4
 MinAndMax nextSmallestAndLargest(unsigned short x)
 {
     MinAndMax result; 
-    // todo
+    int originalCount = countBits(x);
+
+    unsigned short iterDown = x;
+    while (iterDown > 0)
+    {
+        iterDown--;     
+        if (countBits(iterDown) == originalCount)
+        {
+            result.min = iterDown;
+            break;
+        }
+    }
+
+    unsigned short iterUp = x;
+    while (iterUp < USHRT_MAX)
+    {
+        iterUp++;
+        if (countBits(iterUp) == originalCount)
+        {
+            result.max = iterUp;
+            break;
+        }
+    }
+
+    result.valid = result.min != 0 && result.max != 0;
     return result;
 }
 
-// 4.5 
+void print(int x)
+{
+    std::cout << x << " -> " 
+        << nextSmallestAndLargest(x).min << " " 
+        << nextSmallestAndLargest(x).max << std::endl;
+}
+
+void testNextSmallestAndLargest()
+{
+    for (int i = 0; i < 20; i++)
+    {
+        print(i);
+    }
+}
+
+// 5.5 
 bool unknownFunction(int n)
 {
     return (n & n-1) == 0;
@@ -30,7 +81,7 @@ void testUnknownFunction()
     }
 }
 
-// 4.6
+// 5.6
 int numBitsToFlip(unsigned short x, unsigned short y)
 {
     int count = 0;
@@ -53,6 +104,8 @@ void testNumBitsToFlip()
 
 int main(int argc, char * argv[])
 {
+    testNextSmallestAndLargest();
     // testUnknownFunction();
+    // testNumBitsToFlip();
     return 0;
 }
