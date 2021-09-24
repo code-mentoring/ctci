d077c5411871efe6a38b9771afde73d2a6b870c0
edgul <ed.guloien@gmail.com>
2021-09-23 21:46:42 -0400

    cleaned up ed's triple step a bit

--- ch8-recursion/edgul/tripleStep/main.cpp
+++ ch8-recursion/edgul/tripleStep/main.cpp
@@ -10,7 +10,6 @@ int waysToClimbStairs(int n)
     else if (n == 1) return 1;
     else if (n == 2) return 2;
     else if (n == 3) return 4;
-    
     return waysToClimbStairs(n-1) +
            waysToClimbStairs(n-2) +
            waysToClimbStairs(n-3);
@@ -35,23 +34,24 @@ int waysToClimbStairsMemoized(int n)
                    waysToClimbStairsMemoized(n-2) +
                    waysToClimbStairsMemoized(n-3);
         };
-    
         result.insert(std::pair<int,int>(n, func(n)));
     }
-
     return result.at(n);
 }
 
 // helper function to show the timings 
-void timeFunctionCall(std::function<int(int)> f, int n)
+void timeFunctionCall(std::function<int(int)> func, int n)
 {
-    using timept = std::chrono::time_point<std::chrono::system_clock>;
+    using sysclock = std::chrono::system_clock;
+    using timept = std::chrono::time_point<sysclock>;
 
-    timept start = std::chrono::system_clock::now();
-    int temp = f(n);
-    timept end = std::chrono::system_clock::now();
-
+    // time the function call
+    timept start = sysclock::now();
+    int temp = func(n);
+    timept end = sysclock::now();
     std::chrono::duration<double> elapsed_seconds = end - start;
+
+    // just printing
     std::cout << "f(" << n  << ") -> " << temp 
               << " in " << elapsed_seconds.count() << std::endl;
 }
@@ -59,9 +59,9 @@ void timeFunctionCall(std::function<int(int)> f, int n)
 int main(int argc, char **argv)
 {
     int input = 35;
-    timeFunctionCall(waysToClimbStairs, input); 
-    timeFunctionCall(waysToClimbStairsMemoized, input);
-    timeFunctionCall(waysToClimbStairsMemoized, input);
+    timeFunctionCall(waysToClimbStairs, input); // slow
+    timeFunctionCall(waysToClimbStairsMemoized, input); // fast
+    timeFunctionCall(waysToClimbStairsMemoized, input); // even faster
     return 0;
 }
 
