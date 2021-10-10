1f6d469524eeb1da0c5af79097484f3d5fe65b13
sophieatgithub <sophrossit@icloud.com>
2021-10-10 20:16:23 +0100

    seperation of concerns, fixed indentation

--- ch8-recursion/sophie_triple_step.py
+++ ch8-recursion/sophie_triple_step.py
@@ -3,62 +3,52 @@
 
 def main():
 
- count_ways = 0
+    count_ways = 0
 
- def get_total_steps_from_user():
-     return input("Enter number of steps: ")
+    def get_total_steps_from_user():
+        return input("Enter number of steps: ")
 
- def hop_1(steps_left):
-     steps_left -= 1
-     if steps_left == 0:
-         nonlocal count_ways
-         count_ways += 1
-     elif steps_left < 0:
-         return
-     else:
-         calc_possibilities(steps_left)
+    def update_count(count):
+        if count == 0:
+            nonlocal count_ways
+            count_ways += 1
+        elif count < 0:
+            return
+        else:
+            calc_possibilities(count)
 
- def hop_2(steps_left):
-     steps_left -= 2
-     nonlocal count_ways
-     if steps_left == 0:
-         count_ways += 1
-     elif steps_left < 0:
-         return
-     else:
-         calc_possibilities(steps_left)
+    def hop_1(steps_left):
+        steps_left -= 1
+        update_count(steps_left)
 
- def hop_3(steps_left):
-     steps_left -= 3
-     nonlocal count_ways
-     if steps_left == 0:
-         count_ways += 1
-     elif steps_left < 0:
-         return
-     else:
-         calc_possibilities(steps_left)
+    def hop_2(steps_left):
+        steps_left -= 2
+        update_count(steps_left)
 
- def calc_possibilities(total_steps):
-     if total_steps > 0:
-         hop_1(total_steps)
-         hop_2(total_steps)
-         hop_3(total_steps)
+    def hop_3(steps_left):
+        steps_left -= 3
+        update_count(steps_left)
 
- try:
-     user_input = get_total_steps_from_user()
-     int_total_steps = int(user_input)
-     if int_total_steps > 0:
-         calc_possibilities(int_total_steps)
-         print(count_ways)
-     else:
-         print("Enter positive integer.")
-         main()
+    def calc_possibilities(total_steps):
+        if total_steps > 0:
+            hop_1(total_steps)
+            hop_2(total_steps)
+            hop_3(total_steps)
 
- except ValueError:
-     print("Enter integer.")
-     main()
+    try:
+        user_input = get_total_steps_from_user()
+        int_total_steps = int(user_input)
+        if int_total_steps > 0:
+            calc_possibilities(int_total_steps)
+            print(count_ways)
+        else:
+            print("Enter positive integer.")
+            main()
+
+    except ValueError:
+        print("Enter integer.")
+        main()
 
 
 if __name__ == "__main__":
- main()
- 
+    main()
